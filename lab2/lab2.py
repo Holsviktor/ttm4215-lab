7ed2758573cd6b8cf514d071bc615680ae4ad5cc
mr. pi <engineer@holsvik.party>
2026-09-25 09:11:33 +0200

    feat: finished lab2

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -15,7 +15,10 @@ HUMIDITY_PUBLISH_TOPIC = lambda id : f"ttm4115/group1/{id}/humidity"
 
 STATUS_SUBSCRIBER_TOPIC = "ttm4115/group1/+/status"
 STATUS_PUBLISH_TOPIC = lambda id : f"ttm4115/group1/{id}/status"
-FIX_TOPIC = "ttm4115/group1/fix"
+FIX_TOPIC = "ttm4115/group1/actions/fix"
+POLL_TOPIC = "ttm4115/group1/actions/poll"
+SYSTEM_STATUS_TOPIC = "ttm4115/group1/status"
+
 
 class MQTT_Connection:
     def __init__(self,id, connect_callback=None, message_callback=None):
@@ -95,13 +98,13 @@ def humidity_sensor(id): # Sensor Thread
         
         # Operational
         if mqtt_connection.status == STATUS_OPERATIONAL:
-            humidity = sense.get_humidity()
+            humidity = round(sense.get_humidity())
             mqtt_connection.client.publish(mqtt_connection.humidity_topic, f'{{ "humidity" : {humidity:.2f} }}')
 
         # Disconnected
         elif mqtt_connection.status == STATUS_DISCONNECTED:
             if maybe_reconnect(mqtt_connection):
-                humidity = sense.get_humidity()
+                humidity = round(sense.get_humidity())
                 mqtt_connection.client.publish(mqtt_connection.humidity_topic, f'{{ "humidity" : {humidity:.2f} }}')
                 
         # Sensor failed
@@ -137,17 +140,73 @@ def fixer_thread(): # This thread emulates surveillance test
     while (True):
         error = mqtt_connection.client.publish(FIX_TOPIC, 'You are now fixed!')
         sleep(10)
+
+def poller_thread(): # This thread emulates surveillance test
+    mqtt_connection = MQTT_Connection(None)
+    mqtt_connection.connect()
+    while (True):
+        error = mqtt_connection.client.publish(POLL_TOPIC, 'Its time to print')
+        sleep(5)
 #############################################################################################################################
+_dict_lock = Lock()
+
+HEALTHY_STATE = "SAFE"
+SAFE_FAILURE = "SAFE_FAILURE"
+UNSAFE_FAILURE = "UNSAFE_FAILURE"
+
+current_state = ""
 def aggregator(): # Reads messages and contains the final 2oo3/2oo4 aggregation logic
+    sensor_readings = dict()
     def connected_callback(client, userdata, flags, reason_code, properties):
         print("I am become connected, connector of worlds.")
         subscribe_error = client.subscribe(HUMIDITY_SUBSCRIBER_TOPIC)
+        subscribe_error = client.subscribe(POLL_TOPIC)
         if subscribe_error == 1:
             print(f"Aggregator failed to subscribe to humidity topic: {subscribe_error}")
 
     def message_received_callback(client, userdata, m):
-        print(f"I received a message {m.payload} from {m.topic}")
+        #print(f"I received a message {m.payload} from {m.topic}")
+        with _dict_lock:
+            if m.topic.split('/')[-1] == "humidity":
+                sensor_readings[m.topic] =  m.payload
+                print(f'Added reading: {sensor_readings[m.topic]}')
+                # set the correct humidity reading from that sensor
+            elif m.topic == POLL_TOPIC:
+                #non_disconnected_count = len([v for v in sensor_readings.values() if not v is Node])
+                readings = list(sensor_readings.values())
+                most_common_reading = max(set(readings), key=readings.count)
+                working_count = readings.count(most_common_reading)
+                if working_count >= 3:
+                    current_state = HEALTHY_STATE
+                elif working_count == 2:
+                    current_state = SAFE_FAILURE
+                else:
+                    current_state = UNSAFE_FAILURE
 
+                client.publish(SYSTEM_STATUS_TOPIC, current_state)
+
+                print("number of working sensors: ", working_count)
+                print("Current state: ", current_state)
+                if current_state != UNSAFE_FAILURE:
+                    print("Humidity: ", most_common_reading)
+                else:
+                    print("Could not get a humidity reading.")
+                with open('state-history', 'a') as f:
+                    f.write(f"{current_state}\n")
+
+                # Empty the dictionary
+                #   Used for timeouts
+                for topic in sensor_readings.copy().keys():
+                    reading = sensor_readings[topic]
+                    if reading is None:
+                        continue
+
+                    #print(f"{topic}:{reading}")
+                    sensor_readings[topic] = None
+
+
+    with open('state-history', 'w') as f:
+        f.write('')
     mqtt_connection = MQTT_Connection(
         '+', 
         connect_callback=connected_callback,
@@ -180,7 +239,7 @@ def show_status(i, k):
     sense.set_pixels(_pixel_buffer)
 #############################################################################################################################
 if __name__ == '__main__':
-    for id in range (1,5):
+    for id in range (3):
         # Start all threads and then just wait forever :)
         humidity_sensor_thread = Thread(target=humidity_sensor, args=[id])
         humidity_sensor_thread.start()
@@ -188,6 +247,8 @@ if __name__ == '__main__':
 
     fix_thread = Thread(target=fixer_thread)
     fix_thread.start()
+    poll_thread = Thread(target=poller_thread)
+    poll_thread.start()
 
     subscriber_thread = Thread(target=aggregator)
     subscriber_thread.start()
